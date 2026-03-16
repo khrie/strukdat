f2f9d6cf86ff34e15704485b95b77af9ac42a106
Nabil Khairie <batuah.oke@gmail.com>
2026-03-16 22:52:49 +0700

    Add files via upload

---
+++ meet_3/lulus.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int nilai;
+
+    cout << "Masukkan nilai: ";
+    cin >> nilai;
+
+    if(nilai >= 60){
+        cout << "Lulus";
+    }
+    else{
+        cout << "Tidak Lulus";
+    }
+
+    return 0;
+}
